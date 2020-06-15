d4b6c78590f53b3a80809cec6adbbde0fa5cd760
Edvin CANDON <edvincandon@gmail.com>
2020-06-15 19:01:34 +0200

    Add slugify transformer

---
+++ src/transformers/slugify.ts
@@ -0,0 +1,37 @@
+import { Transform } from 'class-transformer';
+import { isNotEmpty } from 'class-validator';
+import slugify from 'slugify';
+import { BadRequestException } from '@nestjs/common';
+
+type KeysOfType<T, V> = { [K in keyof T]: T[K] extends V ? K : never }[keyof T];
+
+interface ISlugifyOptions<T = any> {
+    /* enforce "slugify-able" keys to reference only string values */
+    keys?: Array<KeysOfType<T, string>>;
+    generate?: (obj: T) => string;
+}
+
+export function SlugTransformer(obj: any, options: ISlugifyOptions) {
+    const seed = (() => {
+        if (typeof options.generate === 'function')
+            return options.generate(obj);
+
+        if (options.keys.length > 0)
+            return options.keys
+                .filter(Boolean)
+                .reduce((str, key) => `${str} ${obj[key] as string}`, '');
+
+        throw new BadRequestException('Unable to slugify');
+    })();
+
+    return slugify(seed);
+}
+
+export function Slugify<T = any>(options: ISlugifyOptions<T>) {
+    return (target: any, key: string) => {
+        Transform((val: any, obj: any) => {
+            if (isNotEmpty(val)) return val;
+            return SlugTransformer(obj, options);
+        })(target, key);
+    };
+}

8467ef59e873be641212364e44633697862e0afd
Edvin CANDON <edvincandon@gmail.com>
2020-06-16 16:50:49 +0200

    Improve Serializable decorator to handle decorated getter properties

--- src/serializer.ts
+++ src/serializer.ts
@@ -2,6 +2,7 @@ import { classToPlain } from 'class-transformer';
 
 export interface ISerializable {
     serialize: () => object;
+    toJSON: () => object;
 }
 
 export function serialize() {
@@ -11,5 +12,22 @@ export function serialize() {
 export function Serializable() {
     return (target: any) => {
         target.prototype.serialize = serialize;
+
+        /* normalize toJSON to handle decorated property accessors */
+        target.prototype.toJSON = function overrideToJSON() {
+            const proto = Object.getPrototypeOf(this);
+
+            return Object.entries(Object.getOwnPropertyDescriptors(proto))
+                .filter(
+                    ([_, descriptor]) => typeof descriptor.get === 'function'
+                )
+                .reduce(
+                    (json, [key, descriptor]) => ({
+                        ...json,
+                        [key]: this[key]
+                    }),
+                    { ...this }
+                );
+        };
     };
 }

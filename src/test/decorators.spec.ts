26e8c4933a7ca13c83059ca801c087622755e763
Rmannn <alex.hermann@pop-code.com>
2021-04-12 02:28:49 +0200

    chore: update dependencies, bump version to 0.11.0 breaking changes: Migrate to class-transformer 0.4.0

--- src/test/decorators.spec.ts
+++ src/test/decorators.spec.ts
@@ -1,5 +1,8 @@
+import { ObjectId } from 'bson';
 import { plainToClass } from 'class-transformer';
 
+import { EntityTest } from './module/entity';
+import { EntityRelationship } from './module/entity.relationship';
 import { EntitySlugTest } from './module/entity.slug';
 
 describe('Slug decorator', () => {
@@ -10,5 +13,32 @@ describe('Slug decorator', () => {
         });
         expect(target.slug).toEqual('john-smith');
         expect(target.slug2).toEqual('john-smith');
+    });
+});
+
+describe('TypeObjectId decorator', () => {
+    test('should keep ObjectId on plainToClass', () => {
+        const target = plainToClass(EntityTest, {
+            _id: new ObjectId()
+        });
+        expect(target._id).toBeInstanceOf(ObjectId);
+    });
+    test('should transform string to ObjectId on plainToClass', () => {
+        const target = plainToClass(EntityTest, {
+            _id: new ObjectId().toHexString()
+        });
+        expect(target._id).toBeInstanceOf(ObjectId);
+    });
+    test('should keep ObjectId on plainToClass with relationship type', () => {
+        const target = plainToClass(EntityRelationship, {
+            relationshipAsReference: new ObjectId()
+        });
+        expect(target.relationshipAsReference).toBeInstanceOf(ObjectId);
+    });
+    test('should transform string to ObjectId on plainToClass with relationship type', () => {
+        const target = plainToClass(EntityRelationship, {
+            relationshipAsReference: new ObjectId().toHexString()
+        });
+        expect(target.relationshipAsReference).toBeInstanceOf(ObjectId);
     });
 });

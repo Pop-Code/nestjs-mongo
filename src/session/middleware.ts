0c7d4390d90228f3e32c95412679b4e1a7813710
Edvin CANDON <edvincandon@gmail.com>
2021-05-05 19:14:58 +0200

    fix: re-use cls context if present

--- src/session/middleware.ts
+++ src/session/middleware.ts
@@ -1,22 +1,17 @@
-import { Inject, Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware } from '@nestjs/common';
 import { createNamespace } from 'cls-hooked';
 import Debug from 'debug';
 import { Request, Response } from 'express';
 
 import { DEBUG, SESSION_LOADER_NAMESPACE } from '../constants';
-import { InjectManager } from '../decorators';
-import { MongoManager } from '../manager';
-import { SessionLoaderService } from './service';
 
 @Injectable()
 export class SessionLoaderMiddleware implements NestMiddleware {
     protected log = Debug(`${DEBUG}:SessionLoaderMiddleware`);
-    @InjectManager() protected readonly em: MongoManager;
-    @Inject(SessionLoaderService)
-    protected readonly service: SessionLoaderService;
+    protected static readonly ns = createNamespace(SESSION_LOADER_NAMESPACE);
 
     use(_: Request, __: Response, next: Function) {
-        createNamespace(SESSION_LOADER_NAMESPACE).run(() => {
+        SessionLoaderMiddleware.ns.run(() => {
             this.log('Running namespace %s', SESSION_LOADER_NAMESPACE);
             next();
         });
